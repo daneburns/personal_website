1a846ffa0cdbd0c68e440d8aef5a5d5a14826b68
daneburns <daneb91@gmail.com>
2022-11-04 17:07:08 -0700

    added some styling and formatting(?)

--- src/Pages/Home_.elm
+++ src/Pages/Home_.elm
@@ -1,11 +1,27 @@
 module Pages.Home_ exposing (page)
 
-import Html
+import Html exposing (Html, div, h1, text)
+import Html.Attributes exposing (class)
 import View exposing (View)
 
 
 page : View msg
 page =
     { title = "Homepage"
-    , body = [ Html.text "Hello, world!" ]
+    , body = [ div [ class "body" ] (grid [ div [] [ heading ] ]) ]
     }
+
+
+neon : Html msg
+neon =
+    h1 [ class "neonText" ] [ text "aww skeet skeet" ]
+
+
+heading : Html msg
+heading =
+    div [] [ h1 [ class "heading fullWidth" ] [ text "Dane Burns" ] ]
+
+
+grid : List (Html msg) -> List (Html msg)
+grid arg =
+    [ div [ class "container" ] arg ]
--- src/Pages/Work.elm
+++ src/Pages/Work.elm
@@ -1,11 +1,16 @@
 module Pages.Work exposing (page)
 
-import Html exposing (Html)
+import Html exposing (Html, div, h1, text)
+import Html.Attributes as Attr
 import View exposing (View)
+
+
+heading arg =
+    div [ Attr.class "heading" ] [ text "hey there friend" ]
 
 
 page : View msg
 page =
     { title = "Pages.Work"
-    , body = [ Html.text "/work" ]
+    , body = [ heading "/work" ]
     }
