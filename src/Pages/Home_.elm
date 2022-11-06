b2d62b86cf83bfb7a386bfb087e7e6e446517eb7
daneburns <daneb91@gmail.com>
2022-11-06 11:07:07 -0700

    playing with css selector compatability

--- src/Pages/Home_.elm
+++ src/Pages/Home_.elm
@@ -1,6 +1,6 @@
 module Pages.Home_ exposing (page)
 
-import Html exposing (Html, div, h1, text)
+import Html exposing (Html, div, h1, text, nav, p)
 import Html.Attributes exposing (class)
 import View exposing (View)
 
@@ -8,7 +8,14 @@ import View exposing (View)
 page : View msg
 page =
     { title = "Homepage"
-    , body = [ div [ class "body" ] (grid [ div [] [ heading ] ]) ]
+    , body =
+        [ div [ class "body" ]
+            [ grid
+                [ navbar
+                , heading
+               ]
+            ]
+        ]
     }
 
 
@@ -19,9 +26,18 @@ neon =
 
 heading : Html msg
 heading =
-    div [] [ h1 [ class "heading fullWidth" ] [ text "Dane Burns" ] ]
+    div [class "grid-1"] [ h1 [ class "heading" ] [ text "Dane Burns" ] ]
 
 
-grid : List (Html msg) -> List (Html msg)
+grid : List (Html msg) -> Html msg
 grid arg =
-    [ div [ class "container" ] arg ]
+    div [ class "container" ] arg
+
+
+navbar : Html msg
+navbar =
+    nav [ class "flex-row  grid-1 nav" ]
+        [ p []  [ text "Home" ]
+        , p [class "middleNav"] [ text "Work" ]
+        , p [] [ text "About" ]
+        ]
